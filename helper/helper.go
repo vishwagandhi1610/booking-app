ee4a11941ff2c9c220375e5f248427b0608290c3
Vishwa Gandhi <111928135+vishwagandhi1610@users.noreply.github.com>
2023-06-25 23:39:11 -0400

    Add files via upload

---
+++ helper/helper.go
@@ -0,0 +1,12 @@
+package helper
+
+import "strings"
+
+func ValidateUser(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+	return isValidEmail, isValidName, isValidTicketNumber
+
+}
